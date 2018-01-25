dc6c7ff4b6805dd93c0b92eb2856b385259e55f9
A-Aron <vale.aaron@gmail.com>
2018-01-25 15:12:07 -0500

    Add working move logic

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ class Square {
   constructor(x,y) {
     this.x = x
     this.y = y
-    this.value = null
-    this.next = null
+    this.value = 0
+    this.next = 0
   }
 }
 
@@ -103,50 +103,165 @@ function redrawBoard() {
   const boardSquares = document.getElementsByClassName('square')
   for (let i = 0; i < gameBoard.length; i++) {
     boardSquares[i].innerText = gameBoard[i].next
+    if (gameBoard[i].next === 0) {
+      boardSquares[i].innerText = null
+    }
     gameBoard[i].value = gameBoard[i].next
   }
 }
 
 function moveLeft() {
-    for (let y = 3; y >= 0; y--) {
-       for (let x = 1; x <= 3; x++) {
-        let current = getSquare(x,y)
-        let left = getSquare(x - 1, y)
-        let left2 = getSquare(x - 2, y)
-        let left3 = getSquare(x - 3, y)
-        if (left.value && !left2 && !left3 && left.value === current.value) {
-          left.next = current.value * 2
-          current.next = null
-        } else if (left.value && left2 && !left3 && left2.value === left.value) {
-          left2.next = left.value * 2
-          left.next = current.value
-        } else if (left.value && left2 && !left3 && left.value === current.value && left2.value !== left.value) {
-          left.next = current.value * 2
-          left2.next = left2.value
-          current.next = null
-        } else if (left.value && left2 && left3 && left.value === current.value) {
-          left.next = current.value * 2
-          current.next = null
-        } else if (!left.value && current.value) {
-          left.next = current.value
-          current.next = null
-        }  else {
-          current.next = current.value
-        }
+  let illegalMoveCtr = 0
+  for (let y = 3; y >= 0; y--) {
+      const left0 = getSquare(0, y)
+      const left1 = getSquare(1, y)
+      const left2 = getSquare(2, y)
+      const left3 = getSquare(3, y)
+      let arr = [left0.value, left1.value, left2.value, left3.value]
+      arr2 = arr.filter((v) => v != 0)
+      for (let i = 0; i < arr2.length; i++) {
+        if (arr2[i + 1] === arr2[i]) {
+          arr2[i] *= 2
+          arr2[i + 1] = 0
+         }
       }
-    }
-    redrawBoard()
+      arr3 = arr2.filter((v) => v != 0)
+      left0.next = arr3[0] || 0
+      left1.next = arr3[1] || 0
+      left2.next = arr3[2] || 0
+      left3.next = arr3[3] || 0
+
+      for (let i = arr3.length; i < arr.length; i++) {
+        arr3.push(0)
+      }
+      if (arr.toString() == arr3.toString()) {
+        illegalMoveCtr++
+      }
+  }
+  if (illegalMoveCtr === 4) {
+    return
+  }
+  redrawBoard()
   addRandomSquare()
+  checkForWinner()
 }
 
 function moveRight() {
+  let illegalMoveCtr = 0
+  for (let y = 3; y >= 0; y--) {
+      const right0 = getSquare(0, y)
+      const right1 = getSquare(1, y)
+      const right2 = getSquare(2, y)
+      const right3 = getSquare(3, y)
+      let arr = [right0.value, right1.value, right2.value, right3.value]
+      arr2 = arr.filter((v) => v != 0)
+      for (let i = arr2.length; i < arr.length; i++) {
+        arr2.unshift(0)
+      }
+      for (let i = arr2.length; i > 0; i--) {
+        if (arr2[i - 1] === arr2[i]) {
+          arr2[i] *= 2
+          arr2[i - 1] = 0
+         }
+      }
 
+      right0.next = arr2[0] || 0
+      right1.next = arr2[1] || 0
+      right2.next = arr2[2] || 0
+      right3.next = arr2[3] || 0
+
+
+      if (arr.toString() == arr2.toString()) {
+        illegalMoveCtr++
+      }
+  }
+  if (illegalMoveCtr === 4) {
+    return
+  }
+  redrawBoard()
+  addRandomSquare()
+  checkForWinner()
 }
 
 function moveUp() {
+  let illegalMoveCtr = 0
+  for (let x = 0; x <= 3; x++) {
+      const up0 = getSquare(x, 0)
+      const up1 = getSquare(x, 1)
+      const up2 = getSquare(x, 2)
+      const up3 = getSquare(x, 3)
+      let arr = [up0.value, up1.value, up2.value, up3.value]
+      arr2 = arr.filter((v) => v != 0)
+      for (let i = arr2.length; i < arr.length; i++) {
+        arr2.unshift(0)
+      }
+      for (let i = arr2.length; i > 0; i--) {
+        if (arr2[i - 1] === arr2[i]) {
+          arr2[i] *= 2
+          arr2[i - 1] = 0
+         }
+      }
 
+      up0.next = arr2[0] || 0
+      up1.next = arr2[1] || 0
+      up2.next = arr2[2] || 0
+      up3.next = arr2[3] || 0
+
+
+      if (arr.toString() == arr2.toString()) {
+        illegalMoveCtr++
+      }
+  }
+  if (illegalMoveCtr === 4) {
+    console.log('NO')
+    return
+  }
+  redrawBoard()
+  addRandomSquare()
+  checkForWinner()
 }
 
 function moveDown() {
+  let illegalMoveCtr = 0
+  for (let x = 0; x <= 3; x++) {
+      const down0 = getSquare(x, 0)
+      const down1 = getSquare(x, 1)
+      const down2 = getSquare(x, 2)
+      const down3 = getSquare(x, 3)
+      let arr = [down0.value, down1.value, down2.value, down3.value]
+      arr2 = arr.filter((v) => v != 0)
+      for (let i = 0; i < arr2.length; i++) {
+        if (arr2[i + 1] === arr2[i]) {
+          arr2[i] *= 2
+          arr2[i + 1] = 0
+         }
+      }
+      arr3 = arr2.filter((v) => v != 0)
+      down0.next = arr3[0] || 0
+      down1.next = arr3[1] || 0
+      down2.next = arr3[2] || 0
+      down3.next = arr3[3] || 0
 
+      for (let i = arr3.length; i < arr.length; i++) {
+        arr3.push(0)
+      }
+      if (arr.toString() == arr3.toString()) {
+        illegalMoveCtr++
+      }
+  }
+  if (illegalMoveCtr === 4) {
+    return
+  }
+  redrawBoard()
+  addRandomSquare()
+  checkForWinner()
+}
+
+function checkForWinner() {
+  const boardSquares = document.getElementsByClassName('square')
+  for (let i = 0; i < boardSquares.length; i++) {
+    if (boardSquares[i].value === 2048) {
+      alert('WINNER!!!!')
+    }
+  }
 }
